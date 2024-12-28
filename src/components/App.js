c8f0ebf67ab08d112f10633dec7c7c2869ca8fd8
Anna shabbagh <afreenshabbagh@gmail.com>
2024-12-28 13:18:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, NavLink, Routes } from 'react-router-dom';
-import ItemDetail from '../components/ItemDetail';
-// Dummy data for items
-const items = [
-  { id: 1, name: 'Item 1', description: 'Description for Item 1' },
-  { id: 2, name: 'Item 2', description: 'Description for Item 2' },
-  { id: 3, name: 'Item 3', description: 'Description for Item 3' },
-];
+import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ItemList from "./ItemList";
+import ItemDetail from "./ItemDetail";
 
-// ItemList component to display the list of items
-const ItemList = () => (
-  <div>
-    <h1>Item List</h1>
-    <ul>
-      {items.map(item => (
-        <li key={item.id}>
-          <NavLink to={`/items/${item.id}`}>{item.name}</NavLink>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
-
-
-
-// App component with React Router
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<ItemList />} />
-      <Route path="/items/:id" element={<ItemDetail items={items} />} />
-    </Routes>
-  </Router>
-);
+const App = () => {
+  return (
+    <div>
+       <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ItemList/>}/>
+          <Route path="/items/:id" element={<ItemDetail/>}/>
+        </Routes>
+       </BrowserRouter>
+    </div>
+  )
+}
 
 export default App;
