2ff266fbb365d35af1d5b6f559be2e38626c7ee6
Anna shabbagh <afreenshabbagh@gmail.com>
2025-01-01 21:11:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,25 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ItemList from "./ItemList";
-import ItemDetail from "./ItemDetail";
+import { Routes, Route } from "react-router-dom";
+import Items from "./Items";
+import Item1 from "./Item1";
+import Item2 from "./Item2";
+import Item3 from "./Item3";
 
 const App = () => {
   return (
     <div>
-       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<ItemList/>}/>
-          <Route path="/items/:id" element={<ItemDetail/>}/>
-        </Routes>
-       </BrowserRouter>
+      
+      {/* Do not remove the main div */}
+      
+      <Routes>
+        <Route path="/" element={<Items />}/>
+          <Route path="/items/1" element={<Item1 />} />
+          <Route path="/items/2" element={<Item2 />} />
+          <Route path="/items/3" element={<Item3 />} />
+        
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
