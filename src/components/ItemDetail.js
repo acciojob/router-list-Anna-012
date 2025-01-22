ccf91c2f318380b6388aebe268dd8b2946567ab8
Anna shabbagh <afreenshabbagh@gmail.com>
2025-01-22 23:11:43 +0530

    Update ItemDetail.js

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,26 +1,14 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React from 'react'
+import { useParams } from 'react-router-dom'
 
-const items = [
-  { id: "1", name: "Item 1", description: "Description for Item 1" },
-  { id: "2", name: "Item 2", description: "Description for Item 2" },
-  { id: "3", name: "Item 3", description: "Description for Item 3" },
-];
-
-function ItemDetail() {
-  const { id } = useParams();
-  const item = items.find((item) => item.id === id);
-
-  if (!item) {
-    return <h2>Item not found</h2>;
-  }
-
+const ItemDetail = () => {
+ const {id} = useParams();
   return (
     <div>
-      <h1>{item.name}</h1>
-      <p>{item.description}</p>
-    </div>
-  );
+        <h1>Item {id}</h1>
+        <p>Description for Item {id}</p>
+        </div>
+  )
 }
 
-export default ItemDetail;
+export default ItemDetail
