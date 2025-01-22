7553bea6131620ae4930d46bb0f5e5905658234c
Anna shabbagh <afreenshabbagh@gmail.com>
2025-01-22 23:12:07 +0530

    Update ItemList.js

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,25 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-const items = [
-  { id: "1", name: "Item 1", description: "Description for Item 1" },
-  { id: "2", name: "Item 2", description: "Description for Item 2" },
-  { id: "3", name: "Item 3", description: "Description for Item 3" },
-];
-
-function ItemList() {
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+const arr = [{id:1,name:"Item 1"},{id:2, name:"Item 2"},{id:3, name:"Item 3"}]
+const ItemList = () => {
   return (
-    <div>
-      <h1>Item List</h1>
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>
-            <Link to={`/item/${item.id}`}>{item.name}</Link>
-          </li>
-        ))}
-      </ul>
+    <div><h1>Item List</h1>
+    {arr.map((item,index)=>(<li key={index}><Link to={`/items/${item.id}`}>{item.name}</Link></li>))}
+    <Outlet/>
     </div>
-  );
+  )
 }
 
-export default ItemList;
+export default ItemList
